3c225ed7cb102dd4ae04acbe43f15e8324aa294e
edahigure <edahigure@gmail.com>
2023-01-16 19:04:11 -0600

    Fix linters

--- modules/myDate.js
+++ modules/myDate.js
@@ -1,18 +1,15 @@
-import { DateTime } from "./luxon.js";
+import { DateTime } from './luxon.js';
 
+const getDate = () => {
+  const navBar = document.querySelector('.navBar');
+  const newItem = document.createElement('div');
+  const timeNow = DateTime.now().toFormat('MMMM dd, yyyy');
 
-const getDate = () =>  {
-    const navBar = document.querySelector('.navBar');
-    const newItem = document.createElement('div');
-    const timeNow = DateTime.now().toFormat('MMMM dd, yyyy'); 
-    console.log ('timeNow',timeNow);
-    
-    newItem.className = `myDate`;
-        newItem.innerHTML = `
+  newItem.className = 'myDate';
+  newItem.innerHTML = `
            ${timeNow}
           `;
-    navBar.appendChild(newItem);
-}
+  navBar.appendChild(newItem);
+};
 
-export {getDate}
-
+export { getDate as default };
